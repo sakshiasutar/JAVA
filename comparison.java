a9ff625946635cfbfe531ac5080692be1f8a2f66
sakshiasutar <105741089+sakshiasutar@users.noreply.github.com>
2023-03-14 18:56:11 +0530

    Create comparison.java
    Write a program to compare two numbers.

---
+++ comparison.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class comparison {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        if(a==b)
+        {
+            System.out.println("Equal");
+        }
+        else if(a>b)
+            {
+                System.out.println("a is greater than b");
+            }
+        else
+            {
+                System.out.println("b is greater than a");
+            }
+        
+    }
+}
