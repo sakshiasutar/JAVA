78bc82357b44005383fd4a3c8b9d14e70dccad55
sakshiasutar <105741089+sakshiasutar@users.noreply.github.com>
2023-08-01 00:05:29 +0530

    Create averagecal.java
    Enter 3 numers from the user and make a function to print their average.

---
+++ averagecal.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class averagecal{
+public static void main(String args[])
+{
+ Scanner scanner=new Scanner(System.in);
+
+ System.out.println("Enter the first Number:");
+ double num1=scanner.nextDouble();
+
+ System.out.println("enter the second number:");
+ double num2=scanner.nextDouble();
+
+ System.out.println("Enter the third number:");
+ double num3=scanner.nextDouble();
+
+ double average=averagecalculater(num1,num2,num3);
+ System.out.println("The Average of the three number is:"+ average);
+
+}
+
+public static double averagecalculater(double num1,double num2,double num3){
+    return(num1+num2+num3)/3.0;
+}
+}
