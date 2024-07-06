edbd04ee1f452655a025195dc79c4528e063022b
sakshi <sakshi@host.docker.internal>
2024-07-06 18:39:52 +0530

    OOPS

---
+++ Practice/src/Java/CountCharacter.java
@@ -0,0 +1,22 @@
+package Java;
+import java.util.Scanner;
+
+public class CountCharacter {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("Enter the String:");
+    	String string = sc.nextLine();
+    	
+    	int count = 0;
+    	
+    	for(int i=0;i<string.length();i++) {
+    		if(string.charAt(i) != ' ') {
+    			count++;
+    		}
+    	}
+    	
+    	System.out.println("Total no of characters in a string:" + count);
+    	
+    	
+    }
+}
---
+++ Practice/src/Java/SortNumbersDesc.java
@@ -0,0 +1,39 @@
+package Java;
+
+import java.util.Scanner;
+
+public class SortNumbersDesc {
+   public static void main(String[] args) {
+	   Scanner sc = new Scanner(System.in);
+	   System.out.println("Enter the size of Array");
+	   int n = sc.nextInt();
+	   int[] arr = new int[n];
+	   int temp = 0;
+	   
+	   System.out.println("Enter the elements in array:");
+	   for(int i=0;i<arr.length;i++) {
+		   arr[i] = sc.nextInt();
+	   }
+	   
+	   System.out.println("Original Array:");
+	   for(int i=0;i<arr.length;i++) {
+		   System.out.print(arr[i]+" ");
+	   }
+	   
+	   for(int i=0;i<arr.length;i++) {
+		   for(int j=i+1;j<arr.length;j++) {
+			   if(arr[i] < arr[j]) {
+				   temp = arr[i];
+				   arr[i] = arr[j];
+				   arr[j] = temp;
+			   }
+		   }
+	   }
+	   
+	   System.out.println();
+	   System.out.println("Array after sorting in descending order:");
+	   for(int i=0;i<arr.length;i++) {
+		   System.out.print(arr[i] + " ");
+	   }
+   }
+}
