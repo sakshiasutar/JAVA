6dc40205d9a959f9be056d220df1a552b4b2355d
sakshi <sakshi@DESKTOP-MSGM031>
2024-08-01 00:55:58 +0530

    Basic programs

---
+++ Practice/src/basic/programming/DifferenceOfSum.java
@@ -0,0 +1,27 @@
+package basic.programming;
+import java.util.Scanner;
+public class DifferenceOfSum {
+
+	public static int difference(int n, int m) {
+		int min = 0;
+		int max = 0;
+		
+		for(int i=1;i<=m;i++) {
+			if(i % n == 0) {
+				min = min + i;
+			}
+			else {
+				max = max + i;
+			}
+		}
+		return Math.abs(max - min);
+	}
+	
+	public static void main(String[] args) {
+		
+	     Scanner sc = new Scanner(System.in);
+	     int n = sc.nextInt();
+	     int m = sc.nextInt();
+	     System.out.println(difference(n,m));
+	}
+}
---
+++ Practice/src/basic/programming/LargeSmallClass.java
@@ -0,0 +1,38 @@
+package basic.programming;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class LargeSmallClass {
+
+	public static int largesmall(int[] arr) {
+		ArrayList<Integer> even = new ArrayList<>();
+		ArrayList<Integer> odd = new ArrayList<>();
+		even.add(arr[0]);
+		
+		for(int i=1;i<arr.length;i++) {
+			if(i%2 == 0) {
+				even.add(arr[i]);
+			}
+			else {
+				odd.add(arr[i]);
+			}
+		}
+		Collections.sort(even);
+		Collections.sort(odd);
+		
+		return even.get(1) + odd.get(1);
+	}
+	
+	public static void main(String[] args){
+	
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println(largesmall(arr));
+}
+}
